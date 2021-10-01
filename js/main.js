847c46cb3511c3e1e2a6cf6ad21d62ea361ea815
Juan Ceballos <juandavi789@hotmail.com>
2021-10-01 10:47:54 -0500

    delete a get function within the book class

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+let myLibrary = [];
 
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
+function addBookToLibrary() {}
