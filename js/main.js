561d2d3ff2df739d47b6b8b8ed44bf68b1ab3e72
Juan Ceballos <juandavi789@hotmail.com>
2021-10-12 19:27:43 -0500

    add remove book functionality

--- js/main.js
+++ js/main.js
@@ -7,22 +7,7 @@ class Book {
     this.pages = pages;
     this.status = status;
   }
-
-  // getInfo() {
-  //   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-  // }
 }
-
-// let book1 = new Book('1984', 'George Orwell', 235, 'yes');
-// let book2 = new Book(
-//   'Cien anos de soledad',
-//   'Gabriel Garcia Marquez',
-//   1000,
-//   'yes'
-// );
-
-// myLibrary.push(book1);
-// myLibrary.push(book2);
 
 function addBookToLibrary() {
   let book = new Book(
@@ -35,54 +20,60 @@ function addBookToLibrary() {
 }
 
 function displayBooks(books) {
-  let Bookindex;
-  for (let i = 0; i < books.length; i++) {
-    Bookindex = i;
-  }
+  let bookIndex = books.length - 1;
+
   const newBook = document.createElement('div');
   newBook.classList.add('book');
   //title
   const bookTitle = document.createElement('div');
   bookTitle.classList.add('book-title');
   const titleTextContent = document.createElement('p');
-  titleTextContent.textContent = books[a]['title'];
+  titleTextContent.textContent = books[bookIndex]['title'];
   bookTitle.appendChild(titleTextContent);
   newBook.appendChild(bookTitle);
   //author
   const bookAuthor = document.createElement('div');
   bookAuthor.classList.add('book-author');
   const authorTextContent = document.createElement('p');
-  authorTextContent.textContent = books[a]['author'];
+  authorTextContent.textContent = books[bookIndex]['author'];
   bookAuthor.appendChild(authorTextContent);
   newBook.appendChild(bookAuthor);
   //pages
   const bookPages = document.createElement('div');
   bookPages.classList.add('book-pages');
   const pagesTextContent = document.createElement('p');
-  pagesTextContent.textContent = books[a]['pages'];
+  pagesTextContent.textContent = books[bookIndex]['pages'];
   bookPages.appendChild(pagesTextContent);
   newBook.appendChild(bookPages);
   //status
   const bookStatus = document.createElement('div');
   bookStatus.classList.add('book-status');
   const statusTextContent = document.createElement('p');
-  statusTextContent.textContent = books[a]['status'];
+  statusTextContent.textContent = books[bookIndex]['status'];
   bookStatus.appendChild(statusTextContent);
   newBook.appendChild(bookStatus);
   //button
   const bookButton = document.createElement('button');
   bookButton.classList.add('book-button');
-  bookButton.setAttribute('book-index', Bookindex);
+  bookButton.setAttribute('data-attribute', bookIndex);
   bookButton.textContent = 'delete book';
   newBook.appendChild(bookButton);
 
   booksContainer.appendChild(newBook);
+
+  bookButton.addEventListener('click', removeBook);
+}
+
+function removeBook(e) {
+  e.target.parentElement.remove();
+  myLibrary.splice(e.target, 1);
 }
 
 const booksContainer = document.querySelector('.books');
 const book = document.querySelector('.book');
 const addBook = document.querySelector('#add-book');
 const displayBook = document.querySelector('#display-book');
+const deleteBook = document.querySelector('.book-button');
 
 addBook.addEventListener('click', (e) => {
   e.preventDefault();
